852bd8b545eb0b2e095261c9be00982594ab5bde
Mihails Gilodo <mgilodo@gmail.com>
2023-08-09 16:35:30 +0300

    initial build

---
+++ gulp-tasks/production/img.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+// const imagemin = require("gulp-imagemin");
+const imagemin = import('gulp-imagemin');
+
+gulp.task("img:docs", function() {
+    return (
+        gulp
+            .src("./build/img/**/*.*")
+            // .pipe(imagemin(''))
+            .pipe(
+                imagemin({
+                    progressive: true,
+                    interlaced: true
+                })
+            )
+            .pipe(gulp.dest("./docs/img/"))
+    );
+});
